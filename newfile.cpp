ef4f1b6992e2fb16e1c51136b0a230e3eeff116b
Pankaj <Pankajadhana97@gmail.com>
2020-11-02 22:07:57 +0530

    Pankaj new branch push

---
+++ newfile.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+/*
+Pankaj Adhana
+Panjab university;
+*/
+
+/* Defined values---------------------------------------------------- */
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define cases  int t=1;cin>>t; while(t--) { solve();} return 0
+/*--------------------------------------------------------------------*/
+using namespace std;
+void solve()
+{
+  cout<<"I code for future and past of bad mankind"<<endl;
+}
+int main() 
+{ 
+    fast_io;
+    cases;
+} 
